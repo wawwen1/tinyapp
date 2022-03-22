7f8bdaed9231b21f49d26e00b597aa99d482b49d
warren roque <warrenroque17@gmail.com>
2022-03-22 23:16:41 +0000

    add urls/new & bodyparser

--- express_server.js
+++ express_server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  'b2xTn2': "http://www.lighthouselabs.ca",
+  "b2xTn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -29,6 +32,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 
 
 app.get("/urls/:shortURL", (req, res) => {
